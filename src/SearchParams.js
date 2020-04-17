dcc843b3cb37880e572c51ea6239bcd141e02c77
abhaasgoyal <abhaas.goyal2018@vitstudent.ac.in>
2020-04-17 10:16:03 +0530

    Created custom hook

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -2,21 +2,12 @@ import React, { useState } from "react";
 import { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
-// Also see template tagging
-// class is a reserved word in JS so <div class = ""> ?
-// without useState const location would be fixed and we couldn't change the input
-// useState gives two things - first parameter is the current value and the second value we get is the new value
-// setlocation is called onChange mein to change the state
 const SearchParams = () => {
-  // all hooks begin in use and useState returns an array 1st parameter is the current state and second is the updater function
   const [location, setLocation] = useState("Seattle, WA");
-  //   Default state is Seattle
-  // More hooks below
-  const [animal, setAnimal] = useState("dog");
-  // React states must not be inside an if condition and must be called in the same order as they were
-  // list of strings to components
-  const [breed, setBreed] = useState("");
   const [breeds, setBreeds] = useState([]);
+  // Custom hooks
+  const [animal, AnimalDropdown] = useDropdown("Animal", "All", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
   return (
     <div className="search-params">
       <h1> {location}</h1>
@@ -30,38 +21,8 @@ const SearchParams = () => {
             onChange={(e) => setLocation(e.target.value)}
           />
         </label>
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            key={animal}
-            value={animal}
-            onChange={(e) => setAnimal(e.target.value)}
-            onBlur={(e) => setAnimal(e.target.value)}
-          >
-            <option> All </option>
-            {ANIMALS.map((animal) => (
-              <option value={animal}> {animal}</option>
-            ))}
-          </select>
-        </label>
-        <label htmlFor="breed">
-          breed
-          <select
-            id="breed"
-            value={breed}
-            onChange={(e) => setBreed(e.target.value)}
-            onBlur={(e) => setBreed(e.target.value)}
-            disable={!breeds.length === 0}
-          >
-            <option> All </option>
-            {breeds.map((breedString) => (
-              <option key={breedString} value={breedString}>
-                {breedString}
-              </option>
-            ))}
-          </select>
-        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button> Submit </button>
       </form>
     </div>
--- src/useDropdown.js
+++ src/useDropdown.js
@@ -1,17 +1,16 @@
-// Generic dropdown hook
 import React, { useState } from "react";
 
 const useDropdown = (label, defaultState, options) => {
-  const [state, setState] = useState(defaultState);
-  const id = `use-dropdown-${label.replace(" ", "").toLowerCase}`;
-  const Dropdown = () => {
+  const [state, updateState] = useState(defaultState);
+  const id = `use-dropdown-${label.replace(" ", "").toLowerCase()}`;
+  const Dropdown = () => (
     <label htmlFor={id}>
       {label}
       <select
         id={id}
         value={state}
-        onChange={(e) => setState(e.target.value)}
-        onBlur={(e) => setState(e.target.value)}
+        onChange={(e) => updateState(e.target.value)}
+        onBlur={(e) => updateState(e.target.value)}
         disabled={options.length === 0}
       >
         <option> All </option>
@@ -21,10 +20,9 @@ const useDropdown = (label, defaultState, options) => {
           </option>
         ))}
       </select>
-    </label>;
-  };
-
-  return [state, Dropdown, setState];
+    </label>
+  );
+  return [state, Dropdown, updateState];
 };
 
 export default useDropdown;
