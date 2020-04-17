880f9e039f6260db5ca2ee7f040bc58d2c27b040
abhaasgoyal <abhaas.goyal2018@vitstudent.ac.in>
2020-04-17 15:57:22 +0530

    Added comments in SearchParams.js

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,8 +1,10 @@
+// breeds is not a dependency since not used inside usefeffect to do a functionality
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
+  // Sets below hooks everytime it renders
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
@@ -18,7 +20,8 @@ const SearchParams = () => {
   }, [animal, setBreeds, setBreed]);
   // setBreed and setBreeds even though they dont depend but ESLINT is going to demand
   // useEffect has two parameters the second one being the things it depends on when to run and the first parameter is the asynchronous function which has to be run
-
+  // Empty array would mean i don't depend on anything load only once ?
+  // Without [] in [animal, setBreeds, setBreed] stuck in infinite loop
   return (
     <div className="search-params">
       <form>
