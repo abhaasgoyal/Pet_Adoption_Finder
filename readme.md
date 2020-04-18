039d175eaccfd1c56efafdc7d285cb48d8726b7d
abhaasgoyal <abhaas.goyal2018@vitstudent.ac.in>
2020-04-18 14:24:26 +0530

    Updated README.md to basic functionality

--- readme.md
+++ readme.md
@@ -1,1 +1,21 @@
-#Pet Adoption finder
+# Pet Adoption finder
+
+## Getting Started
+
+1. Clone this repository
+
+'''
+git clone
+'''
+
+2. Go to the directory and install the required dependencies
+
+'''
+npm install
+'''
+
+3. Run in development mode
+
+'''
+npm run dev
+'''
--- src/app.js
+++ src/app.js
@@ -5,14 +5,8 @@ import SearchParams from "./SearchParams";
 import Details from "./Details";
 import ThemeContext from "./ThemeContext";
 
-// Router implementation react router  ( exact match and does both can use switch to do one )vs reach router ( only does which matches the most by a score)
-// :id is a match variable
-// a ki jagah <Link>
 const App = () => {
   const themeHook = useState("peru");
-  // Hooks se different cuz it takes the whole array being returned
-  // If I useState ka color change then all colors change of all buttons which use it
-  // One particular context mein I will give an object
   return (
     <React.StrictMode>
       <ThemeContext.Provider value={themeHook}>
